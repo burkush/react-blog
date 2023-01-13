80364a5936b0a004ffd43896a3932fde87fea107
burkush <avburkush@gmail.com>
2023-01-13 21:47:04 +0200

    added Context API

---
+++ src/context/DataContext.js
@@ -0,0 +1,45 @@
+import { useState, useEffect, createContext } from 'react';
+import useAxiosFetch from '../hooks/useAxiosFetch';
+
+const DataContext = createContext({});
+
+export const DataProvider = ({ children }) => {
+  const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const API_URL = 'http://localhost:3500/posts';
+  const { data, fetchError, isLoading } = useAxiosFetch(API_URL);
+
+  useEffect(() => {
+    setPosts(data);
+  }, [data]);
+
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      (post) =>
+        post.body.toLowerCase().trim().includes(search.toLowerCase().trim()) ||
+        post.title.toLowerCase().trim().includes(search.toLowerCase().trim())
+    );
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  return (
+    <DataContext.Provider
+      value={{
+        search,
+        setSearch,
+        searchResults,
+        fetchError,
+        isLoading,
+        posts,
+        setPosts,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+export default DataContext;
